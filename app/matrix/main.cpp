8a377529c1e0278c770741f11e11dcbfece7157f
Viktor Volkov <megachupakabra007@mail.ru>
2018-02-06 11:59:13 +0300

    created custom iterator for testing puproses

--- app/matrix/main.cpp
+++ app/matrix/main.cpp
@@ -2,9 +2,9 @@
 #include <vector> 
 #include <cassert> 
 #include <algorithm>
+#include <utility>
 
-
-
+/*
 template <typename T>
 class UnboundArray
 {        
@@ -23,6 +23,13 @@ public:
     {
         elementData.reserve(initialCapacity);
     }
+    
+    
+    bool operator==(const UnboundArray& other)
+    {
+        return this == &other;
+    }
+    
     
     T& operator[] (const int &index)
     {
@@ -99,6 +106,15 @@ public:
         return mSize == 0;    
     }
     
+    std::size_t indexOf(const T &el)
+    {
+        auto it = find(elementData.begin(), elementData.end(), el);
+        if (it != elementData.end()) {
+            return distance(elementData.begin(), it);
+        }
+        return -1; 
+    }
+    
     
     std::size_t size() const
     {
@@ -142,7 +158,7 @@ class SpareArray : public UnboundArray<T>
 {
 private:
 
-    UnboundArray<int> indices;
+    UnboundArray<std::size_t> indices;
     
 public:
 
@@ -155,8 +171,14 @@ public:
     T& get(const int &index)
     {
          assert(index >= 0);
-         std::size_t index_of_index = indexOf(index);
-         return UnboundArray<T>::get(index_of_index);         
+         std::size_t pos = indexOfValue(index);
+         return UnboundArray<T>::get(pos);         
+    }
+    
+    
+    std::size_t indexOfValue(const int &index)
+    {
+        return indices.indexOf(index);
     }
     
 
@@ -167,16 +189,6 @@ public:
     }    
     
     
-    std::size_t indexOf(const int &index)
-    {
-        auto it = find(indices.begin(), indices.end(), index);
-        if (it != indices.end()) {
-            return distance(indices.begin(), it);
-        }
-        return -1; 
-    }
-    
-        
     void append(const T &el)
     {
         UnboundArray<T>::append(el);
@@ -194,9 +206,9 @@ public:
         }
         else 
         {
-            std::size_t index_of_index = indexOf(index);
-            UnboundArray<T>::remove(index_of_index);
-            UnboundArray<T>::insert(index_of_index, el);
+            std::size_t pos = indexOfValue(index);
+            UnboundArray<T>::remove(pos);
+            UnboundArray<T>::insert(pos, el);
         }
     }
     
@@ -209,6 +221,13 @@ public:
                                  std::max_element(indices.begin(), indices.end()));
         }
         return -1;
+    }
+    
+    
+    void remove(const int &index)
+    {
+        UnboundArray<T>::remove(index);
+        indices.remove(index);
     }
     
     
@@ -237,8 +256,8 @@ public:
         {
             if (self.indexExists(index)) 
             {
-                std::size_t index_of_index = self.indexOf(index);
-                self.remove(index_of_index);
+                std::size_t pos = self.indexOfValue(index);
+                self.remove(pos);
             }
             return *(new T(EMPTY_ELEMENT));   
         }        
@@ -289,34 +308,162 @@ public:
             sz += row.size();        
         }
         return sz;
-    }  
+    }
+    
+    
+    auto begin()
+    {
+        return std::begin(values);    
+    }
+    
+    
+    auto end()
+    {
+        return std::end(values);    
+    }
+    
+    
+    auto begin() const
+    {
+        return std::cbegin(values);    
+    }
+    
+    
+    auto end() const
+    {
+        return std::cend(values);    
+    }
+      
+    
 };
 
+*/
 
+
+class Simple
+{
+private:
+   std::vector<std::size_t> indices;
+   std::vector<int> values;
+   
+public:
+   
+   void insert(std::size_t index, int value)
+   {
+       indices.push_back(index);
+       values.push_back(value);
+   }
+   
+   int at(std::size_t index)
+   {
+       return values[indices[index]];   
+   }
+   
+   class Iterator
+   {
+   private:
+       const std::vector<std::size_t>* indices;
+       const std::vector<int>* values;
+       std::size_t pos = 0;
+   public:
+       Iterator(const std::vector<std::size_t>* indices_, const std::vector<int>* values_, const std::size_t &pos_ = 0): 
+           values(values_), indices(indices_), pos(pos_){ }
+       
+       bool operator==(const Iterator& other){
+           return this == &other;       
+       }
+       
+       bool operator!=(const Iterator& other){
+           return !operator==(other);       
+       }
+       
+       Iterator operator++() {
+           pos++;
+           Iterator i = *this;
+           return i;       
+       }
+       
+       std::pair<std::size_t, int> operator*()
+       {
+           if (pos < (*values).size())
+           {
+               return std::make_pair((*indices)[pos], (*values)[pos]);          
+           }
+           std::cout << "EMPTY PAIR" << std::endl; //loops infinitely
+           return std::pair<std::size_t,int>{};           
+       }
+       
+       std::pair<std::size_t, int>* operator->()
+       {
+           if (pos < (*values).size()) 
+           {
+               std::pair<std::size_t,int> *p;
+               *p = std::make_pair((*indices)[pos], (*values)[pos]); 
+               return p;       
+           }  
+           return nullptr;      
+       }
+   };
+   
+   Iterator begin() const
+   {
+       return Iterator(&indices, &values, 0);   
+   }
+   
+   Iterator end() const
+   {
+       return Iterator(&indices, &values, values.size());   
+   }
+};
 
 int main() {
-    SpareMatrix<int, -1> matrix;
-    assert(matrix.size() == 0);
-    auto a = matrix[0][0];
-    assert(a == -1);
     
-    matrix[100][100] = 314;
-    assert(matrix[100][100] == 314);
-    assert(matrix.size() == 1);
+    Simple s;
+    s.insert(10, 100);
+    std::cout << s.at(10) << std::endl;
+    int i = 0;
+    for (const std::pair<std::size_t, int> &p : s)
+    {
+        std::cout << p.first << " " << p.second << std::endl;   
+        if (i > 3) break;
+        i++; 
+    }    
+    /*
+    SpareMatrix<int, 0> matrix;
     
-    /*matrix[100][100] = 314;
-    matrix[101][101] = -1;
-    std::cout << matrix.size() << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+        matrix[i][i] = i;    
+    }    
     
-    SpareArray<int, -1> arr;
-    arr[1] = 1;
-    arr[100] = 100;
-    std::cout << arr.size() << std::endl;
-    std::cout << arr[0] << std::endl;
-    std::cout << matrix[100][200] << std::endl;
-    arr[100] = -1;
-    arr[1] = -1;
-    std::cout << arr.size() << std::endl;
+    for (int i = 0; i < 10; ++i)
+    {
+        matrix[i][9 - i] = 9 - i;
+    }
+    
+    
+    for (int i = 1; i < 9; ++i)
+    {
+        for (int j = 1; j < 9; ++j)
+        {
+            std::cout << matrix[i][j] << " ";
+        }  
+        std::cout << std::endl;  
+    }
+        
+    
+    std::cout << std::endl;
+    
+    std::cout << "Number of cells occupied: " << matrix.size() << std::endl;
+    
+    std::cout << std::endl;
+    
+    for (const auto &row : matrix)
+    {
+        //std::cout << matrix.index(row) << std::endl;
+    }   
+    
     */
+    
     return 0;
 }
